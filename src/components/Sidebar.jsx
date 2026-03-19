3817b4c54599516cfacb15eb6f8c70963d00aad3
Amil Zeynalli <amil@Amils-MacBook-Pro.local>
2026-03-19 11:24:45 -0400

    Settings structure created

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+function Sidebar()
+{
+    return(
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Settings">Settings</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+
+export default Sidebar;
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,15 @@
+import {Router} from "react-router-dom";
+import Sidebar from "../components/Sidebar.jsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+function Settings()
+{
+    return (
+        <div>
+            <h1>Edit Profile</h1>
+        </div>
+    )
+}
+
+export default Settings;
