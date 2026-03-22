fd17b13f2a206c3f072dc83bf616554ccc30590e
briansanchez1 <115597244+briansanchez1@users.noreply.github.com>
2026-03-22 19:15:24 -0400

    added temporary landing page, boilerplate login and register pages

---
+++ src/pages/register.jsx
@@ -0,0 +1,56 @@
+import InputField from "../components/InputField";
+import { Link } from "react-router-dom";
+
+function Register() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div
+        style={{
+          background: "#fff",
+          borderRadius: 10,
+          border: "1px solid black",
+          padding: "40px",
+          width: "350px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "12px",
+        }}
+      >
+        <h2 style={{ color: "black" }}>Create Account</h2>
+
+        <InputField type="email" placeholder="Email" />
+        <InputField type="password" placeholder="Password" />
+        <InputField type="password" placeholder="Confirm Password" />
+        <button
+          style={{
+            padding: "10px",
+            borderRadius: 5,
+            border: "none",
+            background: "#4A90E2",
+            color: "#fff",
+            fontWeight: "bold",
+            fontSize: 16,
+            cursor: "pointer",
+          }}
+        >
+          Register
+        </button>
+
+        <div>
+          <Link to="/login" style={{ color: "grey", fontSize: "16px", textDecoration: "None" }}>
+            Already have an account?
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Register;
